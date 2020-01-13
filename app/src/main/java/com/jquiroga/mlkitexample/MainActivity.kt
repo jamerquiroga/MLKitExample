a0bc8a5a692876f2b53025350abf8fa2e4391119
Jamer Quiroga <jamer.quiroga@tismart.com>
2020-01-13 16:23:12 -0500

    Initial Commit

---
+++ app/src/main/java/com/jquiroga/mlkitexample/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.jquiroga.mlkitexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jquiroga.mlkitexample.features.ScanBarcodeActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnBarCodeScanner.setOnClickListener {
+            val intent = Intent(this, ScanBarcodeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/jquiroga/mlkitexample/features/ScanBarcodeActivity.kt
@@ -0,0 +1,13 @@
+package com.jquiroga.mlkitexample.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jquiroga.mlkitexample.R
+
+class ScanBarcodeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_barcode)
+    }
+}
